f768218b444e056eb07acd624ae284d706545551
Robert Popescu <robert.popescu@radcom.ro>
2023-09-10 18:37:59 +0300

    goldan button

--- src/components/header.component/header.component.js
+++ src/components/header.component/header.component.js
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
-            <Navbar.Brand href="/" style={{"color": 'gold'}}>
+            <Navbar.Brand href="/home" style={{"color": 'gold'}}>
                 <FontAwesomeIcon icon={faVideoSlash}/>Gold
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
