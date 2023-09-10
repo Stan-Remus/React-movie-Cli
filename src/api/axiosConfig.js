380f4dfb7c64f46b7696c491292c317d00b64638
Robert Popescu <robert.popescu@radcom.ro>
2023-09-10 17:32:47 +0300

    mongodb url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://draconian-advertisement-production.up.railway.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
